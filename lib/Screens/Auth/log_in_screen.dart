f0321e0f9e7d05906d765eaacdcd57a15cbeb6bf
Ahmed Saad <a7md.s3d.2001@gmail.com>
2021-09-02 10:04:40 +0200

    simple edit

--- lib/Screens/Auth/log_in_screen.dart
+++ lib/Screens/Auth/log_in_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:govet_clinics_dashboard/Provider/model_hud.dart';
+import 'package:govet_clinics_dashboard/Screens/Auth/sign_up_screen.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:provider/provider.dart';
 
@@ -14,7 +15,46 @@ class LogInScreen extends StatelessWidget {
       inAsyncCall: Provider.of<ModelHud>(context).isLoading,
       child: Scaffold(
         appBar: AppBar(),
-        body: Text('Log In'),
+        body: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.0),
+              border: Border.all(color: Colors.black),
+            ),
+            width: width * 0.7,
+            height: height * 0.7,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: height * 0.01,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('If you don\'t have an account'),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => SignUpScreen(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
